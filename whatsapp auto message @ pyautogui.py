3246c5ac861e39933b29d73a7de75ebf0fa9dad7
nani-stark-3000 <101336955+nani-stark-3000@users.noreply.github.com>
2022-11-11 00:29:06 +0530

    Add files via upload

---
+++ whatsapp auto message @ pyautogui.py
@@ -0,0 +1,27 @@
+import pyautogui as pg
+import time
+import pywhatkit as pw
+import datetime
+import random
+
+c_time= datetime.datetime.now()                 # store plesent time in a variable 
+h = c_time.hour
+m = c_time.minute+2                             # set minuts to present time + 2
+
+phone= '+91'+(input("Enter phone number to send message : "))       # phone number input
+
+n=int(input("Enter no.of messages to be sent : "))
+
+t=int(input("Enter delay time : "))
+
+pw.sendwhatmsg(phone,'hello',h,m)           # Open whatsapp web and set courser at require  contact page 
+
+a=["Hello","Hi","Hay","Good Morning","Good Night","Good Evening","Good Afternoon"]
+# list of messages
+
+c = len(a)-1
+for i in range(n):
+    b = random.randint(0,c)         # Selecting a random index in list
+    pg.typewrite(a[b])              # Typing text
+    time.sleep(t)
+    pg.press("enter")               # Enter to send
